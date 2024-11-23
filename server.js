c77cbf3de33cf3998c92f93e5bb2ea556f7de7ac
Your Name <harrismuizz10@gmail.com>
2024-11-23 11:41:50 +0100

    correcting env for mongo dep

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use("/orders", orderRoutes);
 
 //database initialization
 const PORT = process.env.PORT || 5000;
-const DATABASE_URL = process.env.DATABASE_URL;
+const DATABASE_URL = process.env.MONGO_DB_URL;
 mongoose
   .connect(DATABASE_URL)
   .then(() =>
